dce162f3a65fa9a2b5451bd94562947342f096bd
Tiberrr <145925912+Tiber-R@users.noreply.github.com>
2025-03-28 11:18:55 +0100

    Update calculatrice.py

--- calculatrice.py
+++ calculatrice.py
@@ -1,8 +1,4 @@
-
 class Calculatrice:
-    def __init__(self):
-        pass
-
     def addition(self, a, b):
         return a + b
 
@@ -13,6 +9,16 @@ class Calculatrice:
         return a * b
 
     def division(self, a, b):
-        if b == 0:
-            return "Erreur : Division par zéro"
-        return a / b
+        if b != 0:
+            return a / b
+        else:
+            raise ValueError("Division par zéro impossible")
+
+    def puissance(self, a, b):
+        return a ** b
+
+    def racine(self, a):
+        if a >= 0:
+            return a ** 0.5
+        else:
+            raise ValueError("La racine carrée d'un nombre négatif est impossible")
